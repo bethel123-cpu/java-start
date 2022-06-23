c52e062ec771be6dc5c27eb196d063feef09933f
Mpaogbuji bethel <bethelogbuji84@gmail.com>
2022-06-23 02:43:13 +0100

    addedd document.write function

--- main.js
+++ main.js
@@ -11,3 +11,5 @@ console.log(height)
 console.log(country)
 
 alert(` Hello my name is ${fullname} i am ${height} tall, and i am a ${country}`)
+
+document.write (` Hello my name is ${fullname} i am ${height} tall, and i am a ${country}`)
