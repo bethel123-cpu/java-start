4b752259b66d374e2d68d521ea77dc9840e28edb
Mpaogbuji bethel <bethelogbuji84@gmail.com>
2022-06-23 01:13:51 +0100

    javascript project

---
+++ main.js
@@ -0,0 +1,12 @@
+let fullname = "Mpaogbuji Bethel"
+
+let height = " 6.2"
+
+let country = "Nigerian"
+
+
+
+console.log(fullname)
+console.log(height)
+console.log(country)
+window.print(fullname, height, country)
